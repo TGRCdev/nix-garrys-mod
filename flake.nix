f51afe4fd494c5994ac116d485f120595e1967f0
TGRCDev <tgrc@tgrc.dev>
2024-02-15 00:25:05 -0800

    Working on fetching Steam runtimes

--- flake.nix
+++ flake.nix
@@ -3,9 +3,16 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
     pkgsi686Linux = pkgs.pkgsi686Linux;
-    fetchDepot = (pkgs.callPackage ./depots {}).fetchDepot;
+    fetchDepot = (pkgs.callPackage ./depot.nix {}).fetchDepot;
+    fetchRuntime = (pkgs.callPackage ./runtime.nix {}).fetchRuntime;
   in {
-    packages.${system}.garrys-mod = rec {
+    packages.${system} = rec {
+      steam-runtime-heavy = fetchRuntime {
+        runtime = "heavy";
+        snapshot = "0.20231127.68518";
+        filename = "steam-runtime-heavy.tar.xz";
+        hash = "sha256-HvM95MBXmZ1WNhPiRi3Sd7iYTPGa5hNdlKcmSg8Wj9U=";
+      };
       steam-sdk-redist = fetchDepot {
         name = "steam-sdk-redist";
         appId = 4020;
@@ -18,51 +25,53 @@
         '';
         outputHash = "sha256-iivdtcBaMQIyWkA7O1xtBpP41R+4WrsDS8ISIfr8Os8=";
       };
-      dedicated-server-content = fetchDepot {
-        name = "dedicated-server-content";
-        appId = 4020;
-        depotId = 4021;
-        manifestId = 7918776424147734184;
-        outputHash = "sha256-viuPi6ou5EJRqkFW9jpFm8KZNz9ROjivWx1XBQjRJZc=";
-      };
-      dedicated-server-linux-src = fetchDepot {
-        name = "dedicated-server-linux-src";
-        appId = 4020;
-        depotId = 4023;
-        manifestId = 3728493952843195777;
-        outputHash = "sha256-cwyJzU5+xA8bcsuOBXNbR/bNXAQNVXxEfv6DLviHq6I=";
-      };
-      dedicated-server-linux = let
-        runtimeLibs = with pkgs.pkgsi686Linux; [
-          gcc-unwrapped.lib
-          ncurses5
-          gperftools
-        ];
-      in pkgsi686Linux.stdenv.mkDerivation {
-        name = "dedicated-server-linux";
-        src = dedicated-server-linux-src;
-        buildInputs = runtimeLibs;
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        buildPhase = ''
-          mkdir $out
-          ln -s $src/* $out/
-          rm $out/srcds_linux
-          cp $src/srcds_linux $out/srcds_linux
-        '';
-        preFixup = ''
-          patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/srcds_linux
-        '';
-        postFixup = ''
-          wrapProgram $out/srcds_linux --set LD_LIBRARY_PATH "$out:$out/bin:${pkgs.lib.makeLibraryPath runtimeLibs}"
-        '';
-      };
-      default = pkgs.symlinkJoin {
-        name = "garrys-mod-server";
-        paths = [
-          steam-sdk-redist
-          dedicated-server-content
-          dedicated-server-linux
-        ];
+      garrys-mod = rec {
+        dedicated-server-content = fetchDepot {
+          name = "dedicated-server-content";
+          appId = 4020;
+          depotId = 4021;
+          manifestId = 7918776424147734184;
+          outputHash = "sha256-viuPi6ou5EJRqkFW9jpFm8KZNz9ROjivWx1XBQjRJZc=";
+        };
+        dedicated-server-linux-src = fetchDepot {
+          name = "dedicated-server-linux-src";
+          appId = 4020;
+          depotId = 4023;
+          manifestId = 3728493952843195777;
+          outputHash = "sha256-cwyJzU5+xA8bcsuOBXNbR/bNXAQNVXxEfv6DLviHq6I=";
+        };
+        dedicated-server-linux = let
+          runtimeLibs = with pkgs.pkgsi686Linux; [
+            gcc-unwrapped.lib
+            ncurses5
+            gperftools
+          ];
+        in pkgsi686Linux.stdenv.mkDerivation {
+          name = "dedicated-server-linux";
+          src = dedicated-server-linux-src;
+          buildInputs = runtimeLibs;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          buildPhase = ''
+            mkdir $out
+            ln -s $src/* $out/
+            rm $out/srcds_linux
+            cp $src/srcds_linux $out/srcds_linux
+          '';
+          preFixup = ''
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/srcds_linux
+          '';
+          postFixup = ''
+            wrapProgram $out/srcds_linux --set LD_LIBRARY_PATH "$out:$out/bin:${pkgs.lib.makeLibraryPath runtimeLibs}"
+          '';
+        };
+        default = pkgs.symlinkJoin {
+          name = "garrys-mod-server";
+          paths = [
+            steam-sdk-redist
+            dedicated-server-content
+            dedicated-server-linux
+          ];
+        };
       };
     };
   };
