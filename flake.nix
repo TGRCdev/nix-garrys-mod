1c707e8178bf40dd7b5350df88f57c6fdde22d01
TGRCDev <tgrc@tgrc.dev>
2024-03-24 20:19:58 -0700

    Taking some inspiration from steamcmd docker
    Not working yet. I'll crack you eventually, you bastard software.

--- flake.nix
+++ flake.nix
@@ -5,34 +5,43 @@
     pkgsi686Linux = pkgs.pkgsi686Linux;
     fetchDepot = (pkgs.callPackage ./depot.nix {}).fetchDepot;
     fetchRuntime = (pkgs.callPackage ./runtime.nix {}).fetchRuntime;
+    stdenv = pkgs.stdenvNoCC;
   in {
     packages.${system} = rec {
       steam-sdk-redist = fetchDepot {
-        name = "steam-sdk-redist";
+        name = "steam-sdk-redist-src";
         appId = 4020;
         depotId = 1006;
         manifestId = 4884950798805348056;
-        phases = [ "buildPhase" "fixupPhase" ];
-        fixupPhase = ''
-          chmod +x $out/steamclient.so
-          chmod +x $out/linux64/steamclient.so
-        '';
-        outputHash = "sha256-iivdtcBaMQIyWkA7O1xtBpP41R+4WrsDS8ISIfr8Os8=";
+        outputHash = "sha256-IUoZ0JkpisMY4Pzqg3Bi99MhU6IRbBenQJspzq0PRLE=";
       };
+
+      steam-service = stdenv.mkDerivation {
+        src = steam-sdk-redist;
+
+        name = "steam-service";
+        phases = [ "fixupPhase" ];
+        fixupPhase = ''
+          mkdir -p $out/linux64
+          ln -s $src/steamclient.so $out/steamservice.so
+          ln -s $src/linux64/steamclient.so $out/linux64/steamservice.so
+        '';
+      };
+
       garrys-mod = rec {
         dedicated-server-content = fetchDepot {
           name = "dedicated-server-content";
           appId = 4020;
           depotId = 4021;
-          manifestId = 7918776424147734184;
+          manifestId = 5179858603377479094;
           outputHash = "sha256-viuPi6ou5EJRqkFW9jpFm8KZNz9ROjivWx1XBQjRJZc=";
         };
         dedicated-server-linux-src = fetchDepot {
           name = "dedicated-server-linux-src";
           appId = 4020;
           depotId = 4023;
-          manifestId = 3728493952843195777;
-          outputHash = "sha256-cwyJzU5+xA8bcsuOBXNbR/bNXAQNVXxEfv6DLviHq6I=";
+          manifestId = 1978825540093010308;
+          outputHash = "sha256-QWqoAo+niwhu1Ksju/57bRWfMGaAWhphzMUQRoLzmls=";
         };
         runWrapper = let
           steam-run = (pkgs.steam-run);
@@ -42,6 +51,7 @@
             name = "garrys-mod-server";
             paths = [
               steam-sdk-redist
+              steam-service
               dedicated-server-content
               dedicated-server-linux-src
             ];
@@ -55,8 +65,17 @@
             " >> $out/bin/run-gmod-server
             chmod +x $out/bin/run-gmod-server
           '';
+          meta.mainProgram = "run-gmod-server";
         };
       };
+
+      default = garrys-mod.runWrapper;
+    };
+
+    devShells.${system}.default = pkgs.buildFHSUserEnv {
+      name = "steamcmd-fhs";
+      multiArch = true;
+      multiPkgs = pkgs: with pkgs; [ steamcmd ];
     };
   };
 }
