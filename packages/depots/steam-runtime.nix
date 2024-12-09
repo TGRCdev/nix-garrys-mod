32fd977157861af82259344b9a0dad97445e619a
TGRCDev <tgrc@tgrc.dev>
2024-12-09 15:20:45 -0800

    Include license with runtime

--- packages/depots/steam-runtime.nix
+++ packages/depots/steam-runtime.nix
@@ -3,4 +3,5 @@ fetchRuntime {
   name = "steam-runtime";
   version = "0.20241024.105847";
   hash = "sha256-CWld7ROUd819kbziIPPEkIx2qoedBlYKz0usbuGU/ao=";
+  meta.license = lib.licenses.unfreeRedistributable;
 }
